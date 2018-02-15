0cf5641238087901f992ed37ee64047d38f637e7
Fabien RAHAINGOMANANA <frahaing@e2r7p5.42.fr>
2018-02-15 16:59:54 +0100

    Update GNL

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frahaing <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/24 16:38:19 by frahaing          #+#    #+#             */
+/*   Updated: 2017/12/04 12:42:45 by frahaing         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
